d054fafc038f558230918b1f131c66b6186dbacc
Zulfia Kadarberdieva <zulfiakad@gmail.com>
2020-12-08 18:26:29 +0800

    something for today

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :instructor_package
   has_one :review
   validates :description, presence: true
+   
+  before_create :set_status
+  def set_status
+    self.status = "pending"
+  end
 end
--- app/models/instructor_package.rb
+++ app/models/instructor_package.rb
@@ -1,4 +1,9 @@
 class InstructorPackage < ApplicationRecord
   belongs_to :package
   belongs_to :instructor
+  before_create :set_price
+  
+  def set_price
+    self.price = 0
+  end
 end
