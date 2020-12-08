099d010180a8105c84c386d1cce48249377e3433
Zulfia Kadarberdieva <zulfiakad@gmail.com>
2020-12-08 14:51:46 +0800

    Instructor model changed in order to connect it with Locations, Packages and Languages, connection created, updated form to create an instryuctor profile

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,8 +1,12 @@
 class Instructor < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
+  has_many :instructor_locations
+  has_many :instructor_languages
+  has_many :instructor_packages
   has_many :locations, through: :instructor_locations
   has_many :languages, through: :instructor_languages
+  has_many :packages, through: :instructor_packages
   has_many_attached :photos
   validates :description, presence: true
 end
