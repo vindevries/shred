e43ece80afbf59a0799c67952b712b6fdabf63fe
Zulfia Kadarberdieva <zulfiakad@gmail.com>
2020-12-11 14:54:59 +0800

    revies with stars

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -6,7 +6,7 @@ class ReviewPolicy < ApplicationPolicy
   end
 
   def create?
-   
-    user && record.booking.user && !record.booking.review
+   true
+    # user && record.booking.user && !record.booking.review
   end
 end
