8b5f1ff6d1e3a0a3cfbf9b1b925680f87a095696
Zulfia Kadarberdieva <zulfiakad@gmail.com>
2020-12-14 11:36:20 +0800

    conflict solving

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -5,9 +5,12 @@ class BookingPolicy < ApplicationPolicy
     end
   end
 
+  def new?
+   true
+  end
+
   def create?
-    # user && !user.instructor
-    true
+   user && !user.instructor
   end
 
   def update?
